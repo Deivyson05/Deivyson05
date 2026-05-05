4bb430b6295d42a7fbd4e7e564d04f415d1ee3a4
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-05 00:01:10 -0300

    fix: corrige prisma config dnv

--- back-end/lib/prisma.ts
+++ back-end/lib/prisma.ts
@@ -1,10 +1,5 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("@prisma/client");
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export const prisma = new PrismaClient();
